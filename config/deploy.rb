aa9285320b58573b76dcdf3c924ee8590fc70d0d
Loukay <abdelmalek3a@gmail.com>
2016-06-28 22:22:37 +0000

    i think we're done

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :application,     'shopifymetrics'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :ssh_options, {:forward_agent => true}
 
 # Don't change these unless you know what you're doing
 set :pty,             false
